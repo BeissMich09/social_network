eb628f114fe53cead26e61b3b64e33cd4b90506d
BeissenMich <blackyellovv@gmail.com>
2020-05-20 17:35:14 +0300

    withRouter, props.match.params

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,11 +4,16 @@ import Profile from "./Profile";
 import * as axios from "axios";
 import { connect } from "react-redux";
 import { setUserProfile } from "../../Redux/profile_reducer";
+import { withRouter } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -27,4 +32,8 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let withURLDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(
+  withURLDataContainerComponent
+);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,9 +6,27 @@ const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
   }
+  console.log("props....", props);
   return (
     <div className={style.profile}>
-      <img src={props.profile.photos.large} alt="" />
+      <div className={style.photo}>
+        <img src={props.profile.photos.large} alt="" />
+      </div>
+      <div className={style.info}>
+        <h3>Information</h3>
+        <p>Full Name: {props.profile.fullName}</p>
+        <p>Facebook: {props.profile.contacts.facebook}</p>
+        <p>
+          Job:
+          {props.profile.lookingForAJob ? (
+            <img src="/work_icon.svg" alt="Ищу" />
+          ) : (
+            <img src="/no_work_icon.svg" alt="Не ищу работу" />
+          )}
+        </p>
+        <p>{props.profile.lookingForAJobDescription}</p>
+        <p>Status: {props.profile.aboutMe}</p>
+      </div>
     </div>
   );
 };
