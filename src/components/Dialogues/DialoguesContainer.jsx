ef62126e52b32c446f102a87d5e179d97eb4751d
BeissenMich <blackyellovv@gmail.com>
2020-06-10 18:37:13 +0300

    High Order Component

--- src/components/Dialogues/DialoguesContainer.jsx
+++ src/components/Dialogues/DialoguesContainer.jsx
@@ -1,24 +1,14 @@
-// import PersonDialogue from "./PersonDialogue/PersonDialogue";
-// import PrivateDialogues from "./PrivateDialogues/PrivateDialogues";
-import { connect } from "react-redux";
 import Dialogues from "./Dialogues";
-
-// const Dialogues = (props) => {
-//   const { userArr, messageArr } = props.dialoguesPage;
-//   return (
-//     <div className={style.dialogues_content}>
-//       <PersonDialogue userArr={userArr} />
-//       <PrivateDialogues messageArr={messageArr} />
-//     </div>
-//   );
-// };
+import { connect } from "react-redux";
+import { withAuthRedirect } from "../../hoc/withAuthRedirect";
 
 let mapStateToProps = (state) => {
   return {
     dialoguesPage: state.dialoguesPage,
-    isAuth: state.auth.isAuth,
   };
 };
 
-const DialoguesConnected = connect(mapStateToProps)(Dialogues);
-export default DialoguesConnected;
+let AuthRedirectComponent = withAuthRedirect(Dialogues);
+
+const DialoguesContainer = connect(mapStateToProps)(AuthRedirectComponent);
+export default DialoguesContainer;
