15854c420f4ae6aa280907ec05fd2e6e8b77c4ee
BeissenMich <blackyellovv@gmail.com>
2020-07-29 20:46:40 +0300

    стилизация Preloadera

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import preloader from "../../../assets/img/Loading-circles-acs-rectangles.webp";
+import style from "./Preloader.module.css"
 
 
 const Preloader=()=>{
     return(
-        <div>
+        <div className={style.item}>
         <img alt="loader" src={preloader} />
         </div>
     )
